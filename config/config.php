4f256a3b9009a9f392a70490a57fe68fc8444f3a
Alicia Perez <aliciaperez@g.harvard.edu>
2013-12-20 20:46:48 -0500

    setting up live database

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', 'p4_hesdwa_biz'); 
+	define('DB_NAME', 'hesdwabi_p4_hesdwa_biz'); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
